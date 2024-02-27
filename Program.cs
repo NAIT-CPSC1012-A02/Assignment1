9c0e5ff3f88bb1aed01e1045b12e7031e7ebe690
mikaelkiefer <mkdcatral@gmail.com>
2024-02-27 13:23:55 -0700

    Added project files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+
+double investAmount;
+double interestRate;
+double interestPerMonth;
+int numYrs;
+double total;
+
+Console.WriteLine("Enter Investment Amount:");
+investAmount = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Annual Interest Rate in Percentage:");
+interestRate = double.Parse(Console.ReadLine());
+
+interestPerMonth = interestRate / 100 / 12;
+
+Console.WriteLine("Enter Number of years:");
+numYrs = int.Parse(Console.ReadLine());
+
+total = investAmount * Math.Pow(1 + interestPerMonth, numYrs * 12);
+
+Console.WriteLine($"Future Value is {total:c}");
